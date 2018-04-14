fb5060cfaa2e55d636b8d00fbba6ae5009309fbe
Bjarne <b.rye.soerensen@gmail.com>
2018-04-14 20:33:55 +0200

    Children and constructor stuff

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,19 +1,17 @@
 import React from "react";
 
-export class Header extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="navbar-nav">
-            <ul className="nav-item nav-link">
-              <li>
-                <a href="#">Home</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    );
-  }
-}
+export const Header = props => {
+  return (
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="navbar-nav">
+          <ul className="nav-item nav-link">
+            <li>
+              <a href="#">{props.homeLink}</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { setTimeout } from "timers";
 
 export class Home extends React.Component {
   constructor(props) {
     super();
     this.state = {
       age: props.initialAge,
-      status: 0
+      status: 0,
+      homeLink: "Changed Link"
     };
+    setTimeout(() => {
+      this.setState({
+        status: "Jeff"
+      });
+    }, 3000);
   }
   onMakeOlder(event) {
     this.setState({
@@ -14,6 +21,9 @@ export class Home extends React.Component {
     });
   }
 
+  onChangeLink() {
+    this.props.changeLink(this.state.homeLink);
+  }
   render() {
     return (
       <div>
@@ -29,6 +39,15 @@ export class Home extends React.Component {
         >
           Make me older
         </button>
+        <hr />
+        <button onClick={this.props.greet} className="btn btn-danger">
+          Greet
+        </button>
+        <hr />
+        <button onClick={this.onChangeLink.bind(this)} className="btn btn-info">
+          {" "}
+          Change Link
+        </button>
       </div>
     );
   }
@@ -38,4 +57,5 @@ export class Home extends React.Component {
 //    name: React.propTypes.string,
 //    age: React.propTypes.number,
 //    user: React.propTypes.object
+//    greet: React.PropTypes.func
 //  };
